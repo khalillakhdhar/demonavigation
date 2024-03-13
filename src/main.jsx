ced1a7cea8bf1804625a81370606e052525f3c9e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-03-13 22:15:15 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import react bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 
